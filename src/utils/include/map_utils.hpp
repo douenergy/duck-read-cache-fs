c492c8fb3d2b75b7a747d3e1eff5f984c914e762
dentiny <dentinyhao@gmail.com>
2025-02-01 04:03:38 -0800

    lru cache implementation (#11)
    Signed-off-by: dentiny <dentinyhao@gmail.com>

---
+++ src/utils/include/map_utils.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <functional>
+#include <utility>
+
+namespace duckdb {
+
+// A hash wrapper made for `std::reference_wrapper`.
+template <typename Hash> struct RefHash : Hash {
+  RefHash() = default;
+  template <typename H> RefHash(H &&h) : Hash(std::forward<H>(h)) {} // NOLINT
+
+  RefHash(const RefHash &) = default;
+  RefHash(RefHash &&) noexcept = default;
+  RefHash &operator=(const RefHash &) = default;
+  RefHash &operator=(RefHash &&) noexcept = default;
+
+  template <typename T>
+  size_t operator()(std::reference_wrapper<const T> val) const {
+    return Hash::operator()(val.get());
+  }
+  template <typename T> size_t operator()(const T &val) const {
+    return Hash::operator()(val);
+  }
+};
+
+// A hash equal wrapper made for `std::reference_wrapper`.
+template <typename Equal> struct RefEq : Equal {
+  RefEq() = default;
+  template <typename Eq>
+  RefEq(Eq &&eq) : Equal(std::forward<Eq>(eq)) {} // NOLINT
+
+  RefEq(const RefEq &) = default;
+  RefEq(RefEq &&) noexcept = default;
+  RefEq &operator=(const RefEq &) = default;
+  RefEq &operator=(RefEq &&) noexcept = default;
+
+  template <typename T1, typename T2>
+  bool operator()(std::reference_wrapper<const T1> lhs,
+                  std::reference_wrapper<const T2> rhs) const {
+    return Equal::operator()(lhs.get(), rhs.get());
+  }
+  template <typename T1, typename T2>
+  bool operator()(const T1 &lhs, std::reference_wrapper<const T2> rhs) const {
+    return Equal::operator()(lhs, rhs.get());
+  }
+  template <typename T1, typename T2>
+  bool operator()(std::reference_wrapper<const T1> lhs, const T2 &rhs) const {
+    return Equal::operator()(lhs.get(), rhs);
+  }
+  template <typename T1, typename T2>
+  bool operator()(const T1 &lhs, const T2 &rhs) const {
+    return Equal::operator()(lhs, rhs);
+  }
+};
+
+} // namespace duckdb
---
+++ unit/test_shared_lru_cache.cpp
@@ -0,0 +1,43 @@
+// This file tests the stale file deletion.
+
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+#include <string>
+
+#include "lru_cache.hpp"
+
+using namespace duckdb; // NOLINT
+
+TEST_CASE("PutAndGetSameKey", "[shared lru test]") {
+  ThreadSafeSharedLruCache<std::string, std::string> cache{1};
+
+  // No value initially.
+  auto val = cache.Get("1");
+  REQUIRE(val == nullptr);
+
+  // Check put and get.
+  cache.Put("1", std::make_shared<std::string>("1"));
+  val = cache.Get("1");
+  REQUIRE(val != nullptr);
+  REQUIRE(*val == "1");
+
+  // Check key eviction.
+  cache.Put("2", std::make_shared<std::string>("2"));
+  val = cache.Get("1");
+  REQUIRE(val == nullptr);
+  val = cache.Get("2");
+  REQUIRE(val != nullptr);
+  REQUIRE(*val == "2");
+
+  // Check deletion.
+  REQUIRE(!cache.Delete("1"));
+  REQUIRE(cache.Delete("2"));
+  val = cache.Get("2");
+  REQUIRE(val == nullptr);
+}
+
+int main(int argc, char **argv) {
+  int result = Catch::Session().run(argc, argv);
+  return result;
+}
