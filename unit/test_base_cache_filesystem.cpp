fa9dde9f983ebb7db5e811182aaf0536e3969cff
dentiny <dentinyhao@gmail.com>
2025-02-09 08:01:21 +0000

    fix cache filesystem for local instance

---
+++ unit/test_base_cache_filesystem.cpp
@@ -0,0 +1,54 @@
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+#include <string>
+
+#include "disk_cache_filesystem.hpp"
+#include "duckdb/common/virtual_file_system.hpp"
+#include "hffs.hpp"
+
+using namespace duckdb; // NOLINT
+
+namespace {
+
+const std::string TEST_CONTENT = "helloworld";
+const std::string TEST_FILEPATH = "/tmp/testfile";
+void CreateTestFile() {
+  auto local_filesystem = LocalFileSystem::CreateLocal();
+  auto file_handle = local_filesystem->OpenFile(
+      TEST_FILEPATH, FileOpenFlags::FILE_FLAGS_WRITE |
+                         FileOpenFlags::FILE_FLAGS_FILE_CREATE_NEW);
+  local_filesystem->Write(*file_handle, const_cast<char *>(TEST_CONTENT.data()),
+                          TEST_CONTENT.length(), /*location=*/0);
+  file_handle->Sync();
+}
+void DeleteTestFile() {
+  LocalFileSystem::CreateLocal()->RemoveFile(TEST_FILEPATH);
+}
+
+} // namespace
+
+// A more ideal unit test would be, we could check hugging face filesystem will
+// be used for certains files.
+TEST_CASE("Test cached filesystem CanHandle", "[base cache filesystem]") {
+  unique_ptr<FileSystem> vfs = make_uniq<VirtualFileSystem>();
+  vfs->RegisterSubSystem(
+      make_uniq<DiskCacheFileSystem>(make_uniq<HuggingFaceFileSystem>()));
+  vfs->RegisterSubSystem(
+      make_uniq<DiskCacheFileSystem>(make_uniq<LocalFileSystem>()));
+
+  // VFS can handle local files with cached local filesystem.
+  auto file_handle =
+      vfs->OpenFile(TEST_FILEPATH, FileOpenFlags::FILE_FLAGS_READ);
+  // Check casting success to make sure disk cache filesystem is selected,
+  // rather than the fallback local filesystem within virtual filesystem.
+  [[maybe_unused]] auto &cached_file_handle =
+      file_handle->Cast<CacheFileSystemHandle>();
+}
+
+int main(int argc, char **argv) {
+  CreateTestFile();
+  int result = Catch::Session().run(argc, argv);
+  DeleteTestFile();
+  return result;
+}
